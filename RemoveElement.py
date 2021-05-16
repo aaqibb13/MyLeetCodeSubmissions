0a587dac7837bf657ed5d2e96d91407da153aa94
Aaqib Bashir <36571725+aaqibb13@users.noreply.github.com>
2021-05-17 02:49:59 +0530

    Create removeElement.py
    This is my first leetcode submission.

---
+++ RemoveElement.py
@@ -0,0 +1,7 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        try:
+            while True:
+                nums.remove(val)
+        except ValueError:
+            pass
