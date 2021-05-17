20161ed8f3724fa0d0379ffb9767965b881b65c0
Aaqib Bashir <36571725+aaqibb13@users.noreply.github.com>
2021-05-18 02:55:51 +0530

    Delete RemoveElement.py

--- RemoveElement.py
+++
@@ -1,9 +0,0 @@
-#Removing a target from a list
-
-class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
-        try:
-            while True:
-                nums.remove(val)
-        except ValueError:
-            pass
