e3a9556c339368b23d42d4cfef9f5eedb432d8d4
Aaqib Bashir <36571725+aaqibb13@users.noreply.github.com>
2021-05-18 02:56:17 +0530

    Delete mergetwosortedlists.py

--- mergetwosortedlists.py
+++
@@ -1,8 +0,0 @@
-# Merging two Lists and returning a Sorted list using Lists
-
-class Solution:
-  def mergeTwoLists(self, l1: List, l2: List) -> List:
-    for hold, val in enumerate(l2):
-      l1.append(val)
-    l1.sort()
-    print(l1)
