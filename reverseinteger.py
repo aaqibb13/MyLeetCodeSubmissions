286356103670d1e572bf22704c69776561020f6d
Aaqib Bashir <36571725+aaqibb13@users.noreply.github.com>
2021-05-18 01:43:22 +0530

    Update reverseinteger.py

--- reverseinteger.py
+++ reverseinteger.py
@@ -1,18 +1,26 @@
 class Solution:
     def reverse(self, x: int) -> int:
         rev_x = 0
+        neg_limit= -0x80000000
+        pos_limit= 0x7fffffff
         if x < 0:
             x = abs(x)
             while x != 0:
                 rem = (x % 10)
                 rev_x = (rev_x * 10) + rem
                 x = x//10
-            return -rev_x
+            if rev_x & 0x7fffffff == rev_x:
+                return -rev_x
+            else:
+                return 0
         elif x > 0:
             while x != 0:
                 rem = (x % 10)
                 rev_x = (rev_x * 10) + rem
                 x = x//10
-            return rev_x
+            if rev_x & 0x7fffffff == rev_x:
+                return rev_x
+            else:
+                return 0
         else:
             return 0
