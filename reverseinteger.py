22774d0eb5118f6b18c5ebc08370190601c47d80
Aaqib Bashir <36571725+aaqibb13@users.noreply.github.com>
2021-05-18 01:51:09 +0530

    Update reverseinteger.py

--- reverseinteger.py
+++ reverseinteger.py
@@ -1,7 +1,7 @@
 class Solution:
     def reverse(self, x: int) -> int:
         rev_x = 0
-        neg_limit= -0x80000000
+#         neg_limit= -0x80000000
         pos_limit= 0x7fffffff
         if x < 0:
             x = abs(x)
@@ -9,7 +9,7 @@ class Solution:
                 rem = (x % 10)
                 rev_x = (rev_x * 10) + rem
                 x = x//10
-            if rev_x & 0x7fffffff == rev_x:
+            if rev_x & pos_limit == rev_x:
                 return -rev_x
             else:
                 return 0
@@ -18,7 +18,7 @@ class Solution:
                 rem = (x % 10)
                 rev_x = (rev_x * 10) + rem
                 x = x//10
-            if rev_x & 0x7fffffff == rev_x:
+            if rev_x & pos_limit == rev_x:
                 return rev_x
             else:
                 return 0
