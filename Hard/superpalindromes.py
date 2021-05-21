5d5c70cb3ee3d70bfb8aff2d140a7780db8d7aae
Aaqib Bashir <36571725+aaqibb13@users.noreply.github.com>
2021-05-21 20:31:10 +0530

    Added description of superpalindromes.py

---
+++ Hard/superpalindromes.py
@@ -0,0 +1,2 @@
+# Check the number of superpalindromes within a range, inclusive of left and right elements.
+# 121 is a superpalindrome, since 11 * 11 = 121, where 121 is a palindrome and a perfect square and 11 is a palindrome as well. 
