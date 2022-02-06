5f2a28dde041b8314ae0f145c8dac7920812520f
Aaqib Bashir <36571725+aaqibb13@users.noreply.github.com>
2022-02-06 23:38:18 +0530

    Create search_insert_position_with_binary_search.go

---
+++ Daily_Problems/search_insert_position_with_binary_search.go
@@ -0,0 +1,5 @@
+// The program implemented with Binary search has complexity of O(log n) compared to O(n) while using Linear search to insert a missing element in an array
+
+func insert_position(nums []int, target int) int {
+//   The Program logic goes here
+}
