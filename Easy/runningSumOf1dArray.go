e4acdbc3c583c0b606587d7397c373627b465fe3
mostlycryptic <36571725+aaqibb13@users.noreply.github.com>
2023-01-19 22:02:58 +0530

    Update and rename running_sum_of_1d_array.go to runningSumOf1dArray.go

--- Easy/runningSumOf1dArray.go
+++ Easy/runningSumOf1dArray.go
@@ -15,11 +15,11 @@
 
 func runningSum(nums []int) []int {
     for i := 0; i < len(nums)-1; i++ {
-		if i < len(nums) {
-			nums[i+1] = nums[i] + nums[i+1]
-		} else {
-			break
-		}
+	if i < len(nums) {
+		nums[i+1] = nums[i] + nums[i+1]
+	} else {
+		break
 	}
-	return nums
+    }
+    return nums
 }
