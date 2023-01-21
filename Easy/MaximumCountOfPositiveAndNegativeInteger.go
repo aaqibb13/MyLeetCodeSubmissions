443cd1e8a52da7957f976cbc032a76993107e271
mostlycryptic <36571725+aaqibb13@users.noreply.github.com>
2023-01-21 22:44:08 +0530

    Create MaximumCountOfPositiveAndNegativeInteger.go

---
+++ Easy/MaximumCountOfPositiveAndNegativeInteger.go
@@ -0,0 +1,45 @@
+// 2529. Maximum Count of Positive Integer and Negative Integer
+
+// Given an array nums sorted in non-decreasing order, return the maximum between the number of positive integers and the number of negative integers.
+
+// In other words, if the number of positive integers in nums is pos and the number of negative integers is neg, then return the maximum of pos and neg.
+// Note that 0 is neither positive nor negative.
+
+ 
+
+// Example 1:
+
+// Input: nums = [-2,-1,-1,1,2,3]
+// Output: 3
+// Explanation: There are 3 positive integers and 3 negative integers. The maximum count among them is 3.
+// Example 2:
+
+// Input: nums = [-3,-2,-1,0,0,1,2]
+// Output: 3
+// Explanation: There are 2 positive integers and 3 negative integers. The maximum count among them is 3.
+// Example 3:
+
+// Input: nums = [5,20,66,1314]
+// Output: 4
+// Explanation: There are 4 positive integers and 0 negative integers. The maximum count among them is 4.
+
+
+func maximumCount(nums []int) int {
+    neg_nums := 0
+    pos_nums := 0
+    for i := 0; i < len(nums); i++ {
+          if nums[i] != 0{
+              isNegative := math.Signbit(float64(nums[i]))
+              if isNegative {
+                  neg_nums += 1
+              } else {
+                  pos_nums += 1
+              }
+          }
+    }
+    if pos_nums > neg_nums {
+          return pos_nums
+      } else {
+          return neg_nums
+      }
+}
