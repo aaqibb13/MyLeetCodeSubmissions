35e4e47e132476c4dd83046815be6a751be0539e
mostlycryptic <36571725+aaqibb13@users.noreply.github.com>
2023-01-29 22:56:11 +0530

    Update firstLetterToAppearTwice.go

--- Easy/firstLetterToAppearTwice.go
+++ Easy/firstLetterToAppearTwice.go
@@ -1,36 +1,55 @@
-2351. First Letter to Appear Twice
-Easy
-630
-33
-Companies
-Given a string s consisting of lowercase English letters, return the first letter to appear twice.
+// 2351. First Letter to Appear Twice
 
-Note:
+// Given a string s consisting of lowercase English letters, return the first letter to appear twice.
 
-A letter a appears twice before another letter b if the second occurrence of a is before the second occurrence of b.
-s will contain at least one letter that appears twice.
+// Note:
+
+// A letter a appears twice before another letter b if the second occurrence of a is before the second occurrence of b.
+// s will contain at least one letter that appears twice.
  
 
-Example 1:
+// Example 1:
 
-Input: s = "abccbaacz"
-Output: "c"
-Explanation:
-The letter 'a' appears on the indexes 0, 5 and 6.
-The letter 'b' appears on the indexes 1 and 4.
-The letter 'c' appears on the indexes 2, 3 and 7.
-The letter 'z' appears on the index 8.
-The letter 'c' is the first letter to appear twice, because out of all the letters the index of its second occurrence is the smallest.
-Example 2:
+// Input: s = "abccbaacz"
+// Output: "c"
+// Explanation:
+// The letter 'a' appears on the indexes 0, 5 and 6.
+// The letter 'b' appears on the indexes 1 and 4.
+// The letter 'c' appears on the indexes 2, 3 and 7.
+// The letter 'z' appears on the index 8.
+// The letter 'c' is the first letter to appear twice, because out of all the letters the index of its second occurrence is the smallest.
+// Example 2:
 
-Input: s = "abcdd"
-Output: "d"
-Explanation:
-The only letter that appears twice is 'd' so we return 'd'.
+// Input: s = "abcdd"
+// Output: "d"
+// Explanation:
+// The only letter that appears twice is 'd' so we return 'd'.
  
 
-Constraints:
+// Constraints:
 
-2 <= s.length <= 100
-s consists of lowercase English letters.
-s has at least one repeated letter.
+// 2 <= s.length <= 100
+// s consists of lowercase English letters.
+// s has at least one repeated letter.
+
+
+
+func repeatedCharacter(s string) byte {
+    hashMap := map[byte]int{}
+    count := 0
+    var result byte
+    for i := 0; i < len(s); i++ {
+      if value, ok := hashMap[byte(s[i])]; ok {
+      value += 1
+      if value == 2 {
+        result = byte(s[i])
+        break
+      }
+      } else {
+        count += 1
+        hashMap[byte(s[i])] = count
+        count = 0
+     }
+    }
+       return result
+}
